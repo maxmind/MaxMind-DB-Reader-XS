031d68668f2bae1b87682b49c7996e45b6e7bee2
Boris Zentner <bzentner@maxmind.com>
2013-03-20 23:45:18 +0100

    Update benchmark script

--- tools/bench.pl
+++ tools/bench.pl
@@ -10,6 +10,7 @@ my @ips = map {
         map { int( rand(256) ) }
         1 .. 4
 } ( 1 .. 5_000 );
+my $max_ips = $#ips;
 
 use MaxMind::DB::Reader;
 use MaxMind::DB::Reader::XS;
@@ -19,14 +20,33 @@ my $file = '/usr/local/share/GeoIP2/city-v6.db';
 my $reader = MaxMind::DB::Reader->new( file => $file ) or die;
 my $reader_xs = MaxMind::DB::Reader::XS->new( file => $file ) or die;
 
+my $fast_reader_xs = MaxMind::DB::Reader::XS->open( $file, 2 ) or die;
+
+use Data::Dumper;
+print Dumper( $fast_reader_xs->lookup_by_ip('24.24.24.24') );
+
+my ( $reader_idx, $reader_xs_idx, $fast_reader_xs_idx ) = ( 0, 0, 0 );
+
 cmpthese(
-    5,
+    -5,
     {
         'reader' => sub {
-            eval { $reader->record_for_address($_) } for @ips;
+            eval {
+                $reader->record_for_address(
+                    $ips[ $reader_idx++ % $max_ips ] );
+            };
         },
         'reader_xs' => sub {
-            eval { $reader_xs->record_for_address($_) } for @ips;
+            eval {
+                $reader_xs->record_for_address(
+                    $ips[ $reader_xs_idx++ % $max_ips ] );
+            };
+        },
+        'fast_reader_xs' => sub {
+            eval {
+                $fast_reader_xs->lookup_by_ip(
+                    $ips[ $fast_reader_xs_idx++ % $max_ips ] );
+            };
         },
     }
 );
