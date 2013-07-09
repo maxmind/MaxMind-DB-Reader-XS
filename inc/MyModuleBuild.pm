09848a9ccaa55f1ca310955d184802ef790ec9b6
Dave Rolsky <autarch@urth.org>
2013-07-09 15:41:48 -0500

    Make sure we generate a dist that builds properly
    We have to pass some extra flags to Module::Build to get it to link against libmaxminddb

---
+++ inc/MyModuleBuild.pm
@@ -0,0 +1,24 @@
+package inc::MyModuleBuild;
+
+use strict;
+use warnings;
+use namespace::autoclean;
+
+use Moose;
+
+extends 'Dist::Zilla::Plugin::ModuleBuild';
+
+around module_build_args => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    my $args = $self->$orig(@_);
+
+    $args->{extra_linker_flags} = ['-lmaxminddb'];
+
+    return $args;
+};
+
+__PACKAGE__->meta()->make_immutable();
+
+1;
