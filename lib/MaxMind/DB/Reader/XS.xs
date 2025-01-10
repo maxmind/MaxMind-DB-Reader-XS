maxmind/MaxMind-DB-Reader-XS

commit e5322e51dde2954566702fcac2c6827bc0017323

free entry data list before croaking on failed calls to get entry data list

lib/MaxMind/DB/Reader/XS.xs
@@ -30,23 +30,23 @@ static SV *decode_simple_value(MMDB_entry_data_list_s **current)
     SV *sv;
     MMDB_entry_data_s entry_data = (*current)->entry_data;
     switch (entry_data.type) {
-        case MMDB_DATA_TYPE_INT32:
-            sv = newSViv(entry_data.int32);
-            break;
-        case MMDB_DATA_TYPE_UINT16:
-            sv = newSVuv(entry_data.uint16);
-            break;
-        case MMDB_DATA_TYPE_UINT32:
-            sv = newSVuv(entry_data.uint32);
-            break;
         case MMDB_DATA_TYPE_BOOLEAN:
             sv = entry_data.boolean ? &PL_sv_yes : &PL_sv_no;
+            break;
+        case MMDB_DATA_TYPE_INT32:
+            sv = newSViv(entry_data.int32);
             break;
         case MMDB_DATA_TYPE_DOUBLE:
             sv = newSVnv(entry_data.double_value);
             break;
         case MMDB_DATA_TYPE_FLOAT:
             sv = newSVnv(entry_data.float_value);
+            break;
+        case MMDB_DATA_TYPE_UINT16:
+            sv = newSVuv(entry_data.uint16);
+            break;
+        case MMDB_DATA_TYPE_UINT32:
+            sv = newSVuv(entry_data.uint32);
             break;
         case MMDB_DATA_TYPE_UINT64:
             sv = newSVuv(entry_data.uint64);
@@ -173,6 +173,7 @@ _raw_metadata(self, mmdb)
     PPCODE:
         int status = MMDB_get_metadata_as_entry_data_list(mmdb, &entry_data_list);
         if (MMDB_SUCCESS != status) {
+            MMDB_free_entry_data_list(entry_data_list);
             croak("MaxMind::DB::Reader::XS Error getting metadata: %d", status);
         }
 
@@ -201,6 +202,7 @@ _lookup_address(self, mmdb, ip_address)
         if (result.found_entry) {
             entry_error = MMDB_get_entry_data_list(&result.entry, &entry_data_list);
             if (MMDB_SUCCESS != entry_error) {
+                MMDB_free_entry_data_list(entry_data_list);
                 croak("MaxMind::DB::Reader::XS Get entry data error looking up %s", ip_address);
             }
             sv = decode_and_free_entry_data_list(entry_data_list);

commit bd1cacdca296866296ec504914884b2ab62e542b

Do not call MMDB_free_entry_data_list() on libmaxminddb failures
After upgrading libmaxminddb from 1.11.0 to 1.12.0,
t/MaxMind/DB/Reader-broken-databases.t started to segfault. A minimal
reproducer is:

    use MaxMind::DB::Reader;
    # Test broken doubles
    my $reader = MaxMind::DB::Reader->new( file => 'maxmind-db/test-data/GeoIP2-City-Test-Broken-Double-Format.mmdb');
    $reader->record_for_address('2001:220::');

The cause was that MaxMind-DB-Reader-XS called
MMDB_free_entry_data_list(entry_data_list) when handling
a MMDB_get_entry_data_list(, &entry_data_list) failure. But
MMDB_get_entry_data_list() did not initialize the pointer in case of
error.

That was not true even before libmaxminddb 1.12.0, but because
previous libmaxminddb versions did not free the memory itself, the
erroneous MMDB_free_entry_data_list() call actually operated on valid
data and no crash occured. When libmaxminddb 1.12.0 started correctly
to clean up on the error, MaxMind-DB-Reader-XS started to double-free
entry_data_list leading to a crash.

This bug was introduced with e5322e51dde2954566702fcac2c6827bc0017323
commit ("free entry data list before croaking on failed calls to get
entry data list").

This patch removes all MMDB_free_entry_data_list() calls when handling
libmaxminddb failures as the pointer is supposed to be undefined in
that case. This patch does not conditionalize this change on
libmaxminddb version because it's better to leak a memory than to
crash after updating libmaxminddb.

lib/MaxMind/DB/Reader/XS.xs
@@ -170,7 +170,6 @@ static void call_data_callback(MMDB_s *mmdb, SV *data_callback,
     int status = MMDB_get_entry_data_list(record_entry, &entry_data_list);
     if (MMDB_SUCCESS != status) {
         const char *error = MMDB_strerror(status);
-        MMDB_free_entry_data_list(entry_data_list);
         croak(
             "MaxMind::DB::Reader::XS - Entry data error looking at offset %i: %s",
             record_entry->offset, error
@@ -312,7 +311,6 @@ _raw_metadata(self, mmdb)
         int status = MMDB_get_metadata_as_entry_data_list(mmdb, &entry_data_list);
         if (MMDB_SUCCESS != status) {
             const char *error = MMDB_strerror(status);
-            MMDB_free_entry_data_list(entry_data_list);
             croak(
                 "MaxMind::DB::Reader::XS - Error getting metadata: %s",
                 error
@@ -355,7 +353,6 @@ __data_for_address(self, mmdb, ip_address)
             get_status = MMDB_get_entry_data_list(&result.entry, &entry_data_list);
             if (MMDB_SUCCESS != get_status) {
                 const char *get_error = MMDB_strerror(get_status);
-                MMDB_free_entry_data_list(entry_data_list);
                 croak(
                     "MaxMind::DB::Reader::XS - Entry data error looking up \"%s\": %s",
                     ip_address, get_error